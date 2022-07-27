9f1c0ba6272df07cd9d1dcc1a13a035d0d661144
Pablo Peña <insomnionebulous@gmail.com>
2022-07-27 02:16:35 -0300

    feat: configure express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.static("public"))
+app.use(express.json())
+
+// Setting
+app.set("port", process.env.PORT || 8080)
+
+// Middleware
+
+// Routes
+app.use("/api", require("./routes/"))
+
+module.exports = app
